4d63774930741d9d973eef209a78b52cecb41054
Ishika Shrivastava <80690119+Ishikashrivastava@users.noreply.github.com>
2023-11-24 23:58:12 +0530

    Update 1027. Longest Arithmetic Subsequence.py

--- 1027. Longest Arithmetic Subsequence.py
+++ 1027. Longest Arithmetic Subsequence.py
@@ -1,22 +1,23 @@
 
-class Solution:
-    def longestArithSeqLength(self, nums: List[int]) -> int:
-        seqs = {}
-        seen = set()
-        for num in nums:
-            
-            for s in seen:
-                step = num-s
-                if (step,s) in seqs:
-                    longest = seqs[step,s] + 1
-                else:
-                    longest = 2
-                if (step,num) in seqs:
-                    seqs[step,num] = max(seqs[step,num],longest)
-                else:
-                    seqs[step,num] = longest
-            if num not in seen:
-                seqs[0,num] = 1
-            seen.add(num)
+class Solution{
 
-        return max([seqs[k] for k in seqs]) 
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        long  sum = 0;
+        long  max = arr[0];
+        
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            max = Math.max(sum, max);
+            
+            if(sum < 0)
+                sum = 0;
+        }
+        
+        return max;
+        
+    }
+    
+}
