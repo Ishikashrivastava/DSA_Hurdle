c5900c4a6bd384b9bcc07a30496b31228847503b
Ishika <ishika.shrivastava2020@vitbhopal.ac.in>
2023-12-23 21:05:36 +0530

    GFG 16

---
+++ findTriplets.java
@@ -0,0 +1,28 @@
+*Complete the function below*/
+
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	public boolean findTriplets(int arr[] , int n)
+    {
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 2; i++) {
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                int sum = arr[i] + arr[l] + arr[r];
+                if (sum == 0) {
+                    return true;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return false;
+    }
+}
